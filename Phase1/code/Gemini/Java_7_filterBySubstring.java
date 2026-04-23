aa8b497b9b4e37da77ea2a698a3d3ea2a1c93d06
Nejra Gutic <114390638+nejra-gutic@users.noreply.github.com>
2026-04-23 20:46:26 +0200

    Create Java_7_filterBySubstring.java

---
+++ Phase1/code/Gemini/Java_7_filterBySubstring.java
@@ -0,0 +1,24 @@
+import java.util.;
+import java.lang.;
+
+class Solution {
+/**
+Filter an input list of strings only for ones that contain given substring
+>>> filterBySubstring(List.of(), "a")
+[]
+>>> filterBySubstring(Arrays.asList("abc", "bacd", "cde", "array"), "a")
+["abc", "bacd", "array"]
+*/
+public List<String> filterBySubstring(List<String> strings, String substring) {
+List<String> result = new ArrayList<>();
+if (strings == null || substring == null) {
+return result;
+}
+for (String s : strings) {
+if (s.contains(substring)) {
+result.add(s);
+}
+}
+return result;
+}
+}
