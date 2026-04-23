399ef1dc299d660f19fe9d0afc526d216c275d7d
Nejra Gutic <114390638+nejra-gutic@users.noreply.github.com>
2026-04-23 23:09:44 +0200

    Create Java_28_concatenate.java

---
+++ Phase1/code/Gemini/Java_28_concatenate.java
@@ -0,0 +1,22 @@
+import java.util.;
+import java.lang.;
+
+class Solution {
+/**
+Concatenate list of strings into a single string
+>>> concatenate(List.of())
+""
+>>> concatenate(Arrays.asList("a", "b", "c"))
+"abc"
+*/
+public String concatenate(List strings) {
+if (strings == null || strings.isEmpty()) {
+return "";
+}
+StringBuilder sb = new StringBuilder();
+for (String s : strings) {
+sb.append(s);
+}
+return sb.toString();
+}
+}
