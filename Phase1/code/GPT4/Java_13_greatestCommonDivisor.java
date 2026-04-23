2449f5f0c9406f67b0229f6f59a091e6216e5c63
Nejra Gutic <114390638+nejra-gutic@users.noreply.github.com>
2026-04-23 21:35:05 +0200

    Create Java_13_greatestCommonDivisor.java

---
+++ Phase1/code/GPT4/Java_13_greatestCommonDivisor.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+
+class Solution {
+    /**
+    Return a greatest common divisor of two integers a and b
+    >>> greatestCommonDivisor(3, 5)
+    1
+    >>> greatestCommonDivisor(25, 15)
+    5
+     */
+    public int greatestCommonDivisor(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return Math.abs(a);
+    }
+}
