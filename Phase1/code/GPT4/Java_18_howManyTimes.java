4c5058a637a196a6925433c23875950d3f2e3661
Nejra Gutic <114390638+nejra-gutic@users.noreply.github.com>
2026-04-23 21:40:29 +0200

    Create Java_18_howManyTimes.java

---
+++ Phase1/code/GPT4/Java_18_howManyTimes.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+
+class Solution {
+    /**
+    Find how many times a given substring can be found in the original string. Count overlaping cases.
+    >>> howManyTimes("", "a")
+    0
+    >>> howManyTimes("aaa", "a")
+    3
+    >>> howManyTimes("aaaa", "aa")
+    3
+     */
+    public int howManyTimes(String string, String substring) {
+        if (substring.length() == 0 || string.length() == 0 || substring.length() > string.length()) {
+            return 0;
+        }
+
+        int count = 0;
+        for (int i = 0; i <= string.length() - substring.length(); i++) {
+            if (string.substring(i, i + substring.length()).equals(substring)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
